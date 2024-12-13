27973ed3867d744d3b453306182dc9f5b7b219f2
Pranamya Jain <146780116+pranamyajainn@users.noreply.github.com>
2024-12-13 21:28:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -217,7 +217,7 @@ def preview_report():
 
 @app.route('/')
 def index():
-    return render_template("upload.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
